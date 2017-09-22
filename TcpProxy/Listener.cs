557637eb3f6937c7dd4a596fbacc071992a8447d
Gorbachev <config.add@gmail.com>
2017-09-22 18:06:25 +0300

    Инициализация проекта

---
+++ TcpProxy/Listener.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TcpProxy
+{
+    public class Listener
+    {
+        public async Task Listen(CancellationToken cancellationToken)
+        {
+            var listener = new TcpListener(IPAddress.Any, 6060);
+            ThreadPool.QueueUserWorkItem();
+            while (!cancellationToken.IsCancellationRequested)
+            listener.
+        }
+    }
+}
